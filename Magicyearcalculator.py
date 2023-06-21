550fbb9d3b4c5d79e180fc56a9bf2548c8083fee
nickLin990 <nicholas.lin849@gmail.com>
2023-06-21 16:51:19 +1000

    new ver

--- Magicyearcalculator.py
+++ Magicyearcalculator.py
@@ -1,9 +1,10 @@
+calculate_magic_year 
 magic_number = 10
 print("Magic Year Calculator")
-name = input("what is your full name \n")
+name = input("what is your name")
+Surname = input("what is your surname:")
 year_of_birth = int(input("What is your year of birth: "))
 annual_salary = input("What is your annaul salary")
 work_start_year = input("What is your work start year")
-magic_year = year_of_birth + magic_number
-print("Hello, " + name + "!")
-print("Your magic year is:", magic_year)
+monthly_salary = annual_salary / 12
+magic_year = calculate_magic_year(work_start_year)
