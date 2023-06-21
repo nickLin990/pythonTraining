6ec8b89813215ff371525e52b71eb2ae58110898
nickLin990 <nicholas.lin849@gmail.com>
2023-06-21 15:46:09 +1000

    work for today

---
+++ Magicyearcalculator.py
@@ -0,0 +1,9 @@
+magic_number = 10
+print("Magic Year Calculator")
+name = input("what is your full name \n")
+year_of_birth = int(input("What is your year of birth: "))
+annual_salary = input("What is your annaul salary")
+work_start_year = input("What is your work start year")
+magic_year = year_of_birth + magic_number
+print("Hello, " + name + "!")
+print("Your magic year is:", magic_year)
--- Taskeight.py
+++ Taskeight.py
@@ -1,21 +1,19 @@
-numberStringInt = input("guess the secret number \n")
-number = int(numberStringInt)
+import random
 secret_number = (1, 10)
-previous_guess = None  
-guess_count = 0
 next_guesses = set()
+playing = True
 
-nextgenius = 43
-while True:
-   guess = input("Guess the secret number (between 1 and 10): ")
+while playing == True:
+   guess = int(input("Guess the secret number (between 1 and 10): "))
 
    if guess in next_guesses:
         print("You already guessed that number. Try again.")
-        continue
-   next_guesses.add(guess) 
-if guess < secret_number:
+   elif guess < secret_number:
+        next_guesses.add(guess) 
         print("your number is too small, try again.")
-if guess > secret_number:
+   elif guess > secret_number:
+        next_guesses.add(guess) 
         print("your number is too large, try again.")   
-else:
+   else:
         print("good job You guessed the secret number")
+        playing = False
