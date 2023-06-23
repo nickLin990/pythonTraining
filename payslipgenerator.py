b4f68ba5c8b64755d0250f2e2f046d8f702ea498
nickLin990 <nicholas.lin849@gmail.com>
2023-06-23 13:35:28 +1000

    the new task

--- payslipgenerator.py
+++ payslipgenerator.py
@@ -4,3 +4,10 @@
 #ask user for super rate
 #ask user for payment start date
 #ask user for payment end date
+print("Welcome to the payslip generator")
+first_name = input("what is your first name? \n")
+last_name = input("what is your last name \n")
+annual_salary = input("what is your annual salary? \n")
+super_rate = input("what is your super rate? \n")
+payment_start_date = input("what is your payment start date? \n")
+payment_end_date = input("what is your payment end date? \n")
