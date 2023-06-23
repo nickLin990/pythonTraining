b184f146e611b63eecd7f6b927655f8ae017be52
nickLin990 <nicholas.lin849@gmail.com>
2023-06-23 19:53:46 +1000

    new ver

--- payslipgenerator.py
+++ payslipgenerator.py
@@ -4,6 +4,7 @@
 #ask user for super rate
 #ask user for payment start date
 #ask user for payment end date
+#print our results
 print("Welcome to the payslip generator")
 first_name = input("what is your first name? \n")
 last_name = input("what is your last name \n")
@@ -11,3 +12,8 @@ annual_salary = input("what is your annual salary? \n")
 super_rate = input("what is your super rate? \n")
 payment_start_date = input("what is your payment start date? \n")
 payment_end_date = input("what is your payment end date? \n")
+
+full_name = first_name + " " +  last_name
+pay_period = payment_start_date, "-", payment_end_date
+gross_income = int(annual_salary) / 12 
+income_tax = 
